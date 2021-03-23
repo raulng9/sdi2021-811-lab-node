c374bdf54cd17f3615166c9dd93dd7b874a68364
raulng9 <salprez4@gmail.com>
2021-03-23 12:24:48 +0100

    SDI-2021-811-7.5-Vistas y motores de plantillas.

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 let express = require("express");
 let app = express();
 
+
+let swig = require("swig");
 let bodyParser = require("body-parser");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -9,8 +11,8 @@ app.use(express.static("public"));
 app.set("port", 8081);
 
 
-require("./routes/rcanciones.js")(app);
-require("./routes/rusuarios.js")(app);
+require("./routes/rcanciones.js")(app,swig);
+require("./routes/rusuarios.js")(app,swig);
 
 app.listen(app.get("port"), function(){
    console.log("Servidor activo");
