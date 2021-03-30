46292e4065a94073a7a23bae01acba92d8f01d2d
raulng9 <salprez4@gmail.com>
2021-03-30 17:45:23 +0200

    SDI-2021-811-8.4-Registro de usuario, autenticación y uso de sesión.

--- routes/rcanciones.js
+++ routes/rcanciones.js
@@ -1,9 +1,13 @@
 module.exports = function(app, swig, gestorBD){
 
     app.get('/canciones/agregar', function(req,res){
-        let respuesta = swig.renderFile('views/bagregar.html',{
 
-        });
+        if ( req.session.usuario == null){
+            res.redirect("/tienda");
+            return;
+        }
+
+        let respuesta = swig.renderFile('views/bagregar.html',{});
         res.send(respuesta);
     });
 
@@ -47,10 +51,17 @@ module.exports = function(app, swig, gestorBD){
   });
 
   app.post("/cancion", function(req,res){
+
+      if ( req.session.usuario == null){
+          res.redirect("/tienda");
+          return;
+      }
+
       let cancion = {
           nombre: req.body.nombre,
           genero: req.body.genero,
-          precio: req.body.precio
+          precio: req.body.precio,
+          autor: req.session.usuario
       }
       gestorBD.insertarCancion(cancion, function(id){
           if (id == null) {
