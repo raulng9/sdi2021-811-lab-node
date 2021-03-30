d93da447f66c60f0390bfc44f6da93d20e7d9adc
raulng9 <salprez4@gmail.com>
2021-03-30 14:30:08 +0200

    SDI-2021-811-8.1-Acceso a datos de Mongo desde Node.js.

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 let express = require("express");
 let app = express();
 
-
+let mongo = require('mongodb');
 let swig = require("swig");
 let bodyParser = require("body-parser");
 app.use(bodyParser.json());
@@ -9,9 +9,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
 app.set("port", 8081);
+app.set('db','mongodb://admin:sdi@tiendamusica-shard-00-00.ilutx.mongodb.net:27017,tiendamusica-shard-00-01.ilutx.mongodb.net:27017,tiendamusica-shard-00-02.ilutx.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-ctmrkc-shard-0&authSource=admin&retryWrites=true&w=majority');
 
 
-require("./routes/rcanciones.js")(app,swig);
+require("./routes/rcanciones.js")(app,swig,mongo);
 require("./routes/rusuarios.js")(app,swig);
 require("./routes/rautores.js")(app,swig);
 
