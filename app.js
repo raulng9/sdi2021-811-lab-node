258aa19d0830d7e2a2360a8fc337cb4b90d3a0c8
raulng9 <salprez4@gmail.com>
2021-04-10 21:41:44 +0200

    SDI-2021-811-9.4-Sistema de compras.

--- app.js
+++ app.js
@@ -39,6 +39,8 @@ routerUsuarioSession.use(function(req, res, next) {
 app.use("/canciones/agregar",routerUsuarioSession);
 app.use("/publicaciones",routerUsuarioSession);
 app.use("/cancion/*",routerUsuarioSession);
+app.use("/compras", routerUsuarioSession);
+
 
 //routerAudios
 let routerAudios = express.Router();
@@ -49,10 +51,21 @@ routerAudios.use(function(req, res, next) {
 
    gestorBD.obtenerCanciones(
        {_id : mongo.ObjectID(idCancion) }, function (canciones) {
-          if(req.session.usuario && canciones[0].autor === req.session.usuario ){
+          if(canciones[0].autor === req.session.usuario){
              next();
           } else {
-             res.redirect("/tienda");
+              let criterio = {
+                usuario: req.session.usuario,
+                  cancionId: mongo.ObjectID(idCancion)
+              };
+              gestorBD.obtenerCompras(criterio, function(compras){
+                 if(compras != null && compras.length > 0){
+                     next();
+                     return;
+                 }else{
+                     res.redirect("/tienda");
+                 }
+              });
           }
        })
 });
