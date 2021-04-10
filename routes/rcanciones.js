23ea1f98dcd04e9994419b9b49e332e1ac401652
raulng9 <salprez4@gmail.com>
2021-04-10 20:44:20 +0200

    SDI-2021-811-9.1-Gestión de colecciones, eliminar documento.

--- routes/rcanciones.js
+++ routes/rcanciones.js
@@ -190,6 +190,17 @@ module.exports = function(app, swig, gestorBD){
         });
     });
 
+    app.get('/cancion/eliminar/:id', function (req, res) {
+        let criterio = { "_id" : gestorBD.mongo.ObjectID(req.params.id) };
+        gestorBD.eliminarCancion(criterio,function(canciones){
+            if ( canciones == null ){
+                res.send(respuesta);
+            } else {
+               res.redirect("/publicaciones");
+            }
+        });
+    });
+
     function paso1ModificarPortada(files, id, callback){
         if (files && files.portada != null) {
             let imagen =files.portada;
