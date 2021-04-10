950579c2588f7e7197b0ea94ef0885ddf0335620
raulng9 <salprez4@gmail.com>
2021-04-10 22:08:38 +0200

    SDI-2021-811-9.6-Manejo de errores y HTTPS.

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 let express = require("express");
 let app = express();
 
+let fs = require('fs');
+let https = require('https');
 
 let expressSession = require('express-session');
 app.use(expressSession({
@@ -129,6 +131,19 @@ app.get('/', function(req,res){
     res.redirect('/tienda');
 });
 
-app.listen(app.get("port"), function(){
-   console.log("Servidor activo");
+
+app.use(function (err, req, res, next) {
+    console.log("Error producido: " + err); // Mostramos el error en consola
+    if (!res.headersSent) {
+        res.status(400);
+        res.send("Recurso no disponible");
+    }
 });
+
+https.createServer({
+        key: fs.readFileSync('certificates/alice.key'),
+        cert: fs.readFileSync('certificates/alice.crt')
+    }, app).listen(app.get('port'), function(){
+        console.log("Servidor activo");
+    }
+);
